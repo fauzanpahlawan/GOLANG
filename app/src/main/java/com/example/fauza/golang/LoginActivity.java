6dfe48ab4db7d0a7df20d8724c8349e999301f29
fauzanpahlawan <fauzan.pahlawan@gmail.com>
2017-11-24 15:28:38 +0700

    Commit 2

--- app/src/main/java/com/example/fauza/golang/LoginActivity.java
+++ app/src/main/java/com/example/fauza/golang/LoginActivity.java
@@ -108,7 +108,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+//                            updateUI(null);
                         }
                         // ...
                     }
