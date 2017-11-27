aee34572d84279729de155bea5f714a8ec38a212
fauzanpahlawan <fauzan.pahlawan@gmail.com>
2017-11-27 17:51:58 +0700

    Commit 2

---
+++ app/src/main/java/com/example/fauza/golang/ListRequestAdapter.java
@@ -0,0 +1,24 @@
+package com.example.fauza.golang;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+
+public class ListRequestAdapter extends RecyclerView.Adapter<ListRequestViewHolder> {
+
+    //TODO Handling requests data
+    @Override
+    public ListRequestViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ListRequestViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/fauza/golang/ListRequestViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.fauza.golang;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ListRequestViewHolder extends RecyclerView.ViewHolder {
+
+    // TODO RecylcerView Items
+    public ListRequestViewHolder(View itemView) {
+        super(itemView);
+    }
+}
