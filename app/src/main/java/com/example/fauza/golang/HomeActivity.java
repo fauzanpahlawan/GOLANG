6f839e4c8571f2d30036140dbf3a1706bf111fae
fauzanpahlawan <fauzan.pahlawan@gmail.com>
2017-11-27 15:48:41 +0700

    Commit 2

--- app/src/main/java/com/example/fauza/golang/HomeActivity.java
+++ app/src/main/java/com/example/fauza/golang/HomeActivity.java
@@ -3,6 +3,9 @@ package com.example.fauza.golang;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -30,6 +33,19 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonLogOut.setOnClickListener(this);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Create Account page? or simply Log Out
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
