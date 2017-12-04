0057e16913cc8ea33055d27e77853014950fd645
fauzanpahlawan <fauzan.pahlawan@gmail.com>
2017-12-04 11:43:18 +0700

    commit

--- app/src/main/java/com/example/fauza/golang/activity/RequestTourGuideActivity.java
+++ app/src/main/java/com/example/fauza/golang/activity/RequestTourGuideActivity.java
@@ -97,29 +97,33 @@ public class RequestTourGuideActivity extends AppCompatActivity implements View.
                             textViewTanggalWisata.getText().toString(),
                             getString(R.string.BELUM_ADA_TOUR_GUIDE)
                     );
-                    final String tourGuideKey = firebaseUtils.getRef()
+                    String tourGuideKey = firebaseUtils.getRef()
                             .child(getString(R.string.tourGuideRequests))
                             .push()
                             .getKey();
-                    firebaseUtils.getRef()
-                            .child(getString(R.string.tourGuideRequests))
-                            .child(tourGuideKey)
-                            .setValue(tourGuideRequest).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                RequestTourGuideActivity.this.finish();
-                                Intent intent = new Intent(RequestTourGuideActivity.this, HomeMemberActivity.class);
-                                intent.putExtra("key", tourGuideKey);
-                                startActivity(intent);
-                            } else {
-                                if (task.getException() != null) {
-                                    Toast.makeText(RequestTourGuideActivity.this, task.getException().getMessage(),
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        }
-                    });
+                    String key = tourGuideKey;
+                    Intent intent = new Intent(RequestTourGuideActivity.this, HomeMemberActivity.class);
+                    intent.putExtra("key", tourGuideKey);
+                    RequestTourGuideActivity.this.startActivity(intent);
+//                    firebaseUtils.getRef()
+//                            .child(getString(R.string.tourGuideRequests))
+//                            .child(tourGuideKey)
+//                            .setValue(tourGuideRequest).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<Void> task) {
+//                            if (task.isSuccessful()) {
+//                                RequestTourGuideActivity.this.finish();
+//                                Intent intent = new Intent(RequestTourGuideActivity.this, HomeMemberActivity.class);
+//                                intent.putExtra("key", tourGuideKey);
+//                                RequestTourGuideActivity.this.startActivity(intent);
+//                            } else {
+//                                if (task.getException() != null) {
+//                                    Toast.makeText(RequestTourGuideActivity.this, task.getException().getMessage(),
+//                                            Toast.LENGTH_SHORT).show();
+//                                }
+//                            }
+//                        }
+//                    });
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
